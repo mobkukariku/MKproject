43d2ec461b8e608818b6a501f54ae809541c4a0a
Damir Tagankhozhaev <155202834+mobkukariku@users.noreply.github.com>
2024-04-21 00:37:43 +0500

    Update Slider.js

--- Slider.js
+++ Slider.js
@@ -1,3 +1,25 @@
+function updateSwiperParams(){
+    var screenWidth = window.innerWidth;
+    var  slidesPerViewl = 3;
+    var spaceBetween = 30;
+
+if(screenWidth<=768){
+    slidesPerView =2
+    spaceBetween = 20;
+}if (screenWidth<=480){
+    slidesPerView =1;
+    spaceBetween = 10;
+}
+
+swiper.params.slidesPerView = slidesPerView;
+swiper.params.spaceBetween=spaceBetween;
+swiper.update();
+
+}
+
+
+
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -6,4 +28,10 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+updateSwiperParams();
+
+window.addEventListener("resize", function(){
+    updateSwiperParams();
+})
 
